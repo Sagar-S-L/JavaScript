8b966be5cb6185139aec03f79e8f3f3f698724bb
Sagar-S-L <143434110+Sagar-S-L@users.noreply.github.com>
2025-01-20 18:07:01 +0000

    added var and datatypes concept

---
+++ 01_Basics/02_Datatype.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all code as a newer version of JS
+
+// alert ( 3 + 3 ); // we are using nodejs not browser
+
+let name = "sagar";
+let age = 24;
+let isLoggedIn = false;
+
+// Datatypes in JS
+
+// number => range -2^53+1 to 2^53-1
+// bigint for greater values
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => the variable declared but not initialized
+// symbol => unique
+
+//Object
+
+console.log(typeof null); //object because null is a object
+console.log(typeof undefined); // undefined is a type in js
