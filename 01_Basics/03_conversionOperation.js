85cf29ff4c42bec6e31b93a866622387794dcf6d
Sagar-S-L <143434110+Sagar-S-L@users.noreply.github.com>
2025-01-21 13:32:18 +0000

    conversion of databases

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+// let score = 33;
+// score = "33abc"; // type is a number but value is NaN if we converted it to number
+// score = null; //type is a object but value is 0
+// score = undefined; //type is undefined but value is NaN
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0 "" => false "sagar" => true
+
+let num=33
+
+let strnum=String(num);
+console.log(strnum);
+console.log(typeof strnum);
